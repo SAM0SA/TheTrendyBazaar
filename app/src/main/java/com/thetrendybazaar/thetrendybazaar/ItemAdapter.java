124468e8c4337d6421b42b15207e9698421f4cc0
skotagiri1998 <skotagiri98@gmail.com>
2019-05-01 17:00:13 -0400

    Created Store Activity

---
+++ app/src/main/java/com/thetrendybazaar/thetrendybazaar/ItemAdapter.java
@@ -0,0 +1,36 @@
+package com.thetrendybazaar.thetrendybazaar;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+    public ItemAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class ItemViewHolder extends RecyclerView.ViewHolder{
+
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+    }
+}
+
--- app/src/main/java/com/thetrendybazaar/thetrendybazaar/StoreActivity.java
+++ app/src/main/java/com/thetrendybazaar/thetrendybazaar/StoreActivity.java
@@ -2,8 +2,13 @@ package com.thetrendybazaar.thetrendybazaar;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class StoreActivity extends AppCompatActivity {
@@ -11,14 +16,41 @@ public class StoreActivity extends AppCompatActivity {
     private NavigationView mNavigationView;
     private MenuItem accountItem;
 
+//    Toolbar
+    private Toolbar toolbar;
+
+//    Layouts
+    private DrawerLayout drawerLayout;
+
+//    Views
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(null,drawerLayout, toolbar,
+                R.string.drawer_open,R.string.drawer_close );
+        toggle.syncState();
+
         mNavigationView = findViewById(R.id.nav_view);
         accountItem = mNavigationView.getMenu().findItem(R.id.drawer_my_account);
         Intent intent = getIntent();
         accountItem.setTitle(intent.getStringExtra("name"));
+
+        recyclerView = findViewById(R.id.items_recycler);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else super.onBackPressed();
     }
 }
